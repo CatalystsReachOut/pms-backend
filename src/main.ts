87b6dc6c86aeeb40ba9cf4216896e3c94a7f544b
prakash singh <prkskrs@gmail.com>
2023-08-20 14:06:02 +0530

    cors changed

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors()
   const config = new DocumentBuilder()
     .setTitle('Catalyts Hub')
     .setDescription('Catalyts Hub API')
